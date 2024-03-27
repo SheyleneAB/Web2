16dc87a90af2a682b3ca45f1e43b70e097a4308f
SheyleneAB <41123357+SheyleneAB@users.noreply.github.com>
2024-03-27 23:40:40 +0100

    colorpicker

---
+++ WebstormProjects/Portfolio/Les 4/Opdracht colorpicker/code.js
@@ -0,0 +1,45 @@
+const setup = () => {
+    let colorDemos=document.getElementsByClassName("colorDemo");
+    let sliders = document.getElementsByClassName("slider");
+
+    // we moeten zowel op het input als het change event reageren,
+    // zie http://stackoverflow.com/questions/18544890
+    for (let i = 0; i<sliders.length; i++) {
+        sliders[i].addEventListener("change", update);
+        sliders[i].addEventListener("input", update);
+    }
+
+
+    // maak het blokje rood
+    colorDemos[0].style.backgroundColor="red";
+}
+
+const update = () => {
+    let sliders = document.getElementsByClassName("slider");
+    let toonWaardeRood = document.getElementById("labelRood");
+    let toonWaardeGroen = document.getElementById("labelGroen");
+    let toonWaardeBlauw = document.getElementById("labelBlauw");
+
+    let roodValue = sliders[0].value;
+    let groenValue = sliders[1].value;
+    let blauwValue = sliders[2].value;
+    let colordemo = document.getElementsByClassName("colorDemo");
+    colordemo[0].style.backgroundColor = `rgb(${roodValue}, ${groenValue}, ${blauwValue})`;
+
+
+    console.log("de waarde van de rode slider is momenteel: " + roodValue);
+    console.log("de waarde van de groene slider is momenteel: " + groenValue);
+    console.log("de waarde van de blauwe slider is momenteel: " + blauwValue);
+    toonWaardeRood.textContent = "Red "+ roodValue;
+    toonWaardeGroen.textContent = "Green "+ groenValue;
+    toonWaardeBlauw.textContent = "Blue " + blauwValue;
+
+}
+
+// dit is de eerste regel code die uitgevoerd wordt,
+// de bovenstaande functie declaraties introduceren
+// enkel de functies en voeren ze niet uit natuurlijk.
+//
+// Onderstaande zorgt ervoor dat de setup functie wordt
+// uitgevoerd zodra de DOM-tree klaar is.
+window.addEventListener("load", setup);
