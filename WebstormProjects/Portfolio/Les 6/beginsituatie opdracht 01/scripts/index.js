fcd154e037dd865ab3928925329fe9f7ed9f2a58
SheyleneAB <41123357+SheyleneAB@users.noreply.github.com>
2024-04-11 15:15:49 +0200

    opdracht 1 les 6

--- WebstormProjects/Portfolio/Les 6/beginsituatie opdracht 01/scripts/index.js
+++ WebstormProjects/Portfolio/Les 6/beginsituatie opdracht 01/scripts/index.js
@@ -1,22 +1,35 @@
 const setup = () => {
 	// registeer click event listener 'voegToe' bij #btnAdd
+    let btnAdd = document.getElementById("btnAdd");
+    btnAdd.addEventListener("click", voegToe);
 
 	// registreer click event listener 'wisAlles' bij #btnClear
+    let btnClear = document.getElementById("btnClear");
+    btnClear.addEventListener("click", wisAlles);
 	
 	// registreer click event listener 'maakBelangrijk' bij elke <li> in .lstIngredients
+    let lstIngredients = document.getElementById("lstIngredients")
+    lstIngredients.addEventListener("click", maakBelangrijk);
 }
 
 const voegToe = () => {
 	// Lees de tekst uit het textveld en voeg nieuw <li> element toe
+    let txtveld = document.getElementById("txtInput")
+    let lstIngredients =document.getElementById("lstIngredients")
+    lstIngredients.innerHTML += `<li>${txtveld.value}</li>`;
 }
 
 const wisAlles = () => {
 	// Wis alle ingredienten
 	// Je kunt dit doen door alle de .innerHTML van #lstIngredients een lege string in te stellen
+    let lstIngredients =document.getElementById("lstIngredients")
+    lstIngredients.innerHTML = "";
 }
 
 const maakBelangrijk = (event) => {
 	// Geef het geklikte element de CSS class 'belangrijk'
+   let item = event.target;
+    item.classList.add("belangrijk");
 }
 	
 window.addEventListener('load',setup);
